9b074f1fd9f1a8608905755bad6c6dfb163fb7d4
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-01-13 16:01:43 +0000

    adding more benchmarks

---
+++ packages/engine/benchmarking/experiments/num_agents/behaviors/init_js/2_fields/src/init.js
@@ -0,0 +1,9 @@
+const init = (context) => {
+  const globals = context.globals();
+
+  return [...new Array(globals.num_agents).keys()].map((_) => ({
+    behaviors: ["empty.js"],
+    field_1: 0,
+    field_2: 0,
+  }));
+};
