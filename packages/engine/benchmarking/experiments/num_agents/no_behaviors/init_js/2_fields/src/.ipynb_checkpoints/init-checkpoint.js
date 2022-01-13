2a8f0957dcac7d44791fc5a933ebbd6b5e24b5ae
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-01-13 15:55:11 +0000

    Removing checpoints

--- packages/engine/benchmarking/experiments/num_agents/no_behaviors/init_js/2_fields/src/.ipynb_checkpoints/init-checkpoint.js
+++
@@ -1,8 +0,0 @@
-const init = (context) => {
-  const globals = context.globals();
-
-  return [...new Array(globals.num_agents).keys()].map((_) => ({
-    field_1: 0,
-    field_2: 0
-  }));
-};
--- packages/engine/benchmarking/experiments/num_agents/no_behaviors/init_js/no_fields/src/.ipynb_checkpoints/init-checkpoint.js
+++
@@ -1,5 +0,0 @@
-const init = (context) => {
-  const globals = context.globals();
-
-  return [...new Array(globals.num_agents).keys()].map((_) => ({}));
-};
