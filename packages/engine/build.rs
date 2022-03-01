59e26d02f0b8cadf3e89576762f143f209c769b5
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-03-01 21:39:39 +0000

    Re-adding stdlib arg

--- packages/engine/build.rs
+++ packages/engine/build.rs
@@ -40,5 +40,6 @@ fn main() {
         .flag("-std=c++14")
         .file("src/worker/runner/javascript/mini_v8/ffi.cc")
         .cpp(true)
+        .cpp_link_stdlib("c++")
         .compile("libmini-v8-ffi.a");
 }
