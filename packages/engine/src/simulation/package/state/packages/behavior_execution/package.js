f05ad20b0bb84bb655ba2d6d9e43150b0244c12a
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-03-01 15:42:55 +0000

    Trying to allocate a 10GB arraybuffer

--- packages/engine/src/simulation/package/state/packages/behavior_execution/package.js
+++ packages/engine/src/simulation/package/state/packages/behavior_execution/package.js
@@ -132,6 +132,8 @@ const load_behaviors = (experiment, behavior_descs) => {
 
 const start_experiment = (experiment, init_message, experiment_context) => {
   const buffer = new ArrayBuffer(10_000_000_000);
+  const view = new Uint8Array(buffer);
+  console.log(view[9_999_999_999]);
   load_behaviors(experiment, init_message);
 };
 
